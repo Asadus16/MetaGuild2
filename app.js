2d78531312574af78d2774a12f28db761f84e01b
hasnatsajid <hasnatsajid965@gmail.com>
2024-05-25 19:07:16 +0500

    Added cors :fire:

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 8000;
 const apiRoutes = require("./src/routes/api");
 
 app.use(express.json());
+
+app.use(
+  cors({
+    // Enable CORS with specific origin
+    origin: "http://localhost:3000", // Allow requests from this origin
+  })
+);
 
 app.use("/", apiRoutes);
 
