7aa6e58bd893f0771ba65270c7b76e02a4219a60
hasnatsajid <hasnatsajid965@gmail.com>
2024-08-11 20:38:58 +0500

    Added mail sending api endpoint :sparkles:

--- src/controllers/mailer.js
+++ src/controllers/mailer.js
@@ -1,34 +1,34 @@
 const sgMail = require("@sendgrid/mail");
+const { MailerSend, EmailParams, Sender, Recipient } = require("mailersend");
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const senderEmail = "dev.hassuu@gmail.com";
-
-async function sendEmail(recipientEmail, subject, body) {
-  const msg = {
-    from: senderEmail,
-    to: recipientEmail,
-    subject,
-    text: body,
-  };
-
-  try {
-    await sgMail.send(msg);
-    console.log("Email sent successfully");
-  } catch (error) {
-    console.error("Error sending email:", error);
-  }
-}
+const mailerSend = new MailerSend({
+  apiKey: process.env.MAILERSEND_API_KEY,
+});
 
 const mailSender = async (req, res) => {
   try {
     const { recipient, subject, body } = req.body;
+    const sentFrom = new Sender(
+      "sender@trial-pr9084znxrj4w63d.mlsender.net",
+      "MetaGuild"
+    );
 
-    const info = await sendEmail(recipient, subject, body);
+    const recipients = [new Recipient(recipient, "MetaUser")];
+
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setTo(recipients)
+      .setReplyTo(sentFrom)
+      .setSubject(subject)
+      .setHtml(body)
+      .setText("This is the text content");
+
+    await mailerSend.email.send(emailParams);
+
     res.json({ message: "Email sent successfully!" });
   } catch (error) {
     console.error("Error sending email:", error);
-    res.status(500).json({ message: "Failed to send email" });
+    res.status(500).json({ message: error });
   }
 };
 
